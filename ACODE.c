f73bce56499b828c45f5f8c8be59496861275ce6
Sarup Banskota <sbanskota08@gmail.com>
2013-10-05 23:08:47 +0530

    Add solution to acode, although it seems to be WA

---
+++ ACODE.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/* Given an encrypted string encrypted according to the rules
+   defined in spoj/acode, finds out the possible number of 
+   decryptions.
+*/
+int isWithin(int a, int b){
+  int num;
+  num = 10*a + b;
+  if(num <= 26) return 1;
+  else return 0;
+}
+
+int main(){
+  
+  char c;
+  int i;		/* current digit */
+  int back=0;           /* previous digit */
+  int pwback=1;         /* number of ways till one digit  back */
+  int pwbackback=0;     /* number of ways till two digits back */
+  int pw=1;             /* number of ways overall */
+
+  while(1){
+		c = getchar();	
+		if (c=='0') break;
+		while (c != '\n'){
+			i = c - '0';
+			if (isWithin(back,i)) pw = pwback + pwbackback;
+			else pw = pwback;
+			back = i;
+			pwbackback = pwback;
+			pwback = pw;
+			c = getchar();
+		}
+		printf("%d\n", pw);
+		back=0;
+		pwback=1;
+		pwbackback=0;
+		pw=1;
+	}
+  return 0;
+}
